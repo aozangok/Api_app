f99c8a567d3c8c5b95dac261f0f7d185d4137543
Ozan Gök <aliozangok@gmx.de>
2020-12-18 07:34:48 +0100

    first prototype

---
+++ myScript.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById('startBtn');
+const bubble = document.getElementById('bubble');
+
+startBtn.addEventListener('click', (e) => {
+  bubble.style.visibility = 'visible';
+});
